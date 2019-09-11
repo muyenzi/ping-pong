d5c28473a972952695757cf0bc5c7bf46b0cf791
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-09-11 10:37:49 +0200

    the pingpong project

--- App.java
+++ App.java
@@ -10,6 +10,7 @@ public class App {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("I'm a ping-pong application!");
         System.out.println("Enter a number:");
+        System.out.println("thanks");
         try {
             String stringUserNumber = bufferedReader.readLine();
             int intUserNumber = Integer.parseInt(stringUserNumber);
